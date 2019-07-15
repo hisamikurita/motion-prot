930e3df0e609dc21beb40e3f9b1510251fa41864
hisami.kurita <47776346+hisamikurita@users.noreply.github.com>
2019-07-16 00:22:08 +0900

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,9 @@
+//MyWorksクリック時の処理
+document.querySelector('.works').addEventListener('click', () => {
+    const works = document.querySelector('.animation_hidden');
+    works.classList.add('active')
+});
+document.querySelector('.works').addEventListener('click', () => {
+    const works = document.querySelector('.animation03');
+    works.classList.add('active')
+});
